573d1c12aef80fbd959d3f0453c420b757719217
Jelena Gavran <jelena.gavran@fer.hr>
2022-01-02 23:10:21 +0100

    Male preinake u views.py i MojaKantica.html
    U views.py odn funkciju mojakantica sam dodala parsiranje heada kad se
    pošalje GET, al inace nis spesl

--- eKantica/eKantica_App/views.py
+++ eKantica/eKantica_App/views.py
@@ -1,19 +1,38 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def home(request):
     return render(request, "Home.html")
 
+@csrf_exempt
 def mojakantica(request):
+    context = {}
     #ovdje će se odvijati obrada podataka 
-    if request.method == "POST":
-        if "ime_post_metode1" in request.POST:
-            pass
-        pass
 
-    return render(request, "MojaKantica.html")
+    if request.method == "GET":
+
+        print("registriram GET")
+
+        http_request = {}
+        for key in request.META:
+            if "HTTP" in key:
+                http_request.update({key:request.META[key]})
+        print(http_request)
+        #npr ako je poslano kljuc:vrijednost, u request.META to izgleda kao HTTP_KLJUC:vrijednost
+
+    if request.method == "POST":
+        print("registriram POST")
+        print(request.POST)
+
+    context["temp"] = 0
+    context["moisture"] = 0
+    context["humidity"] = 0
+    context["water"] = 0
+
+    return render(request, "MojaKantica.html", context)
 
 def info(request):
     return render(request, "Info.html")
